bbd8f214aad4a3a1d2dd560d6ebd84ffc0c24200
big-lip-bob <41026257+big-lip-bob@users.noreply.github.com>
2019-05-09 14:39:30 +0200

    Updated files and started basic editing

--- NuclearCraftio/data.lua
+++ NuclearCraftio/data.lua
@@ -1,1 +1,4 @@
---TODO add require() statements for each file defining entities and such
+--TODO add require() statements to load / execute each file effectively registering them if they use data:extend({})
+
+require("prototypes.groups")
+require("prototypes.technology")
